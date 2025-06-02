ac204b4ba30d522ce117070615e53843b765bfa9
joaorodrigues <joao.rodrigues.jr21@hotmail.com>
2025-06-02 16:10:14 +0100

    Styling - Styled the navbar header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,9 +1,23 @@
-const Header = () => {
+export const Header = () => {
 	return (
 		<header>
-			<h1>Header</h1>
+			<div className='container-md relative'>
+				<div className='header'>
+					<nav>
+						<ul>
+							<li>
+								<a href='/home'>Home</a>
+							</li>
+							<li>
+								<a href='/login'>Browse</a>
+							</li>
+							<li>
+								<a href='/register'>FAQ</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
 		</header>
 	);
 };
-
-export default Header;
